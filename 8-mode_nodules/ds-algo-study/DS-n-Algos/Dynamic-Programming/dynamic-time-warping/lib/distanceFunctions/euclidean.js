8040c09b702bfc1d0d21008c69092ac0a9f2d970
Bryan C Guner <66654881+bgoonz@users.noreply.github.com>
2021-06-17 03:20:08 -0400

    Delete 8-mode_nodules directory

--- 8-mode_nodules/ds-algo-study/DS-n-Algos/Dynamic-Programming/dynamic-time-warping/lib/distanceFunctions/euclidean.js
+++
@@ -1,9 +0,0 @@
-var distance = function ( x, y ) {
-  var difference = x - y;
-  var euclideanDistance = Math.sqrt( difference * difference );
-  return euclideanDistance;
-};
-
-module.exports = {
-  distance: distance
-};
--- 8-mode_nodules/ds-algo-study/DS-n-Algos/Dynamic-Programming/dynamic-time-warping/lib/distanceFunctions/manhattan.js
+++
@@ -1,9 +0,0 @@
-var distance = function ( x, y ) {
-  var difference = x - y;
-  var manhattanDistance = Math.abs( difference );
-  return manhattanDistance;
-};
-
-module.exports = {
-  distance: distance
-};
