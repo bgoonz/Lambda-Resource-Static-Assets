bdb75d4711206b3e1f30baf75fd9d7588fd653bd
bryan <bryan.guner@gmail.com>
2021-06-19 06:33:20 -0400

    update

--- 3-misc/js-examples-master/modules/top-level-await/modules/getColors.js
+++
@@ -1,5 +0,0 @@
-// fetch request
-const colors = fetch('https://mdn.github.io/js-examples/modules/top-level-await/data/colors.json')
-	.then(response => response.json());
-
-export default await colors;
--- 5-Websites/closure-repo/test/test.js
+++
@@ -1,8 +0,0 @@
-const mocha = new Mocha({
-  reporter: "mochawesome",
-  reporterOptions: {
-    overwrite: true,
-    reportTitle: "My Custom Title",
-    showPassed: false,
-  },
-});
